0cfcca43d0ec6a783ba9a088824c7fb9be979226
Muhamad Rian Alpajirin <ryan.cyborg205@gmail.com>
2021-06-04 21:39:45 +0700

    edit pengumuman

--- main/serializers/pengumuman_serializer.py
+++ main/serializers/pengumuman_serializer.py
@@ -3,12 +3,22 @@ from ..models.pengumuman import Pengumuman
 
 class PengumumanSerializer(serializers.ModelSerializer):
 
+    # file_pengumuman = serializers.FileField(allow_empty_file=True)
+    # nama = serializers.CharField()
+    # deskripsi = serializers.CharField()
+
+    # def save(self):
+    #     print(self.validated_data['file_pengumuman'])
+        
+    #     nama = self.validated_data['nama']
+    #     deskripsi = self.validated_data['deskripsi']
+        
     def to_internal_value(self, data):
         file_pengumuman = data.get('file_pengumuman')
 
         return_data = {
             'nama' : data.get('nama'),
-            'deksripsi' : data.get('deskripsi')
+            'deskripsi' : data.get('deskripsi')
         }
 
         if isinstance(file_pengumuman, str):
